0dd167091a19a741af8d5acf15ac5ad938d14643
Yemi Oduyemi <yhermii@gmail.com>
2023-02-23 15:22:15 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle, followed by a new line
+ *@size:is the size of the triangle
+ *Returns: Always 0
+ *
+ *
+ */
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	 return (0);
+}
+
