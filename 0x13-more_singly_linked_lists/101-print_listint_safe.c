23e195aa858be86babd2c48564bc489ddffbe0a6
Yemi Oduyemi <yhermii@gmail.com>
2023-04-03 14:52:10 -0700

    Edited

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list of listint_t nodes
- * @head: pointer to the head of the list
+ * main - check the code
  *
- * Return: the number of nodes in the list
+ * Return: Always 0.
  */
-size_t print_listint_safe(const listint_t *head)
+int main(void)
 {
-	size_t count = 0;
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
 
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
-
-		if (head <= head->next)
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			exit(98);
-		}
-
-		head = head->next;
-	}
-
-	return (count);
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    return (0);
 }
